1562d5d31d7a0d84031884d13567ef9ad14daa84
Aditya <adityabatra277@gmail.com>
2026-05-17 21:45:20 +0100

    chore: remove custom webpack configuration in favor of turbopack

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   serverExternalPackages: ['chromadb', 'onnxruntime-node'],
-  webpack: (config) => {
-    config.watchOptions = {
-      ignored: ['**/node_modules', '**/.next', '**/.git'],
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
